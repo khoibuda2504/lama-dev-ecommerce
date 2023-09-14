b4dee47ea743600b2cc96a716651a004b2e02cc4
dangkhoi2504 <khoi.bd@whysoft.vn>
2023-09-14 16:41:48 +0700

    fix v2

--- src/components/menuCategories/MenuCategories.jsx
+++ src/components/menuCategories/MenuCategories.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './menuCategories.module.css'
 import Link from 'next/link';
 const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/categories", {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/categories`, {
     cache: "no-store",
   });
 
